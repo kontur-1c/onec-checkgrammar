dc72b5becebeb7b3dee351952f1dfee24f3ef03a
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2022-01-26 09:02:12 +0500

    ci: semver

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 import os
-
+import semver
 from setuptools import find_namespace_packages, setup
 
-package_version = os.environ.get("VERSION") or "0.dev0"
+package_version = os.environ.get("VERSION")
 
+if not semver.isvalid(package_version):
+    package_version = "0.0.0-dev"
 with open("README.md", "r", encoding="utf-8") as readme:
     long_description = readme.read()
 
@@ -14,7 +16,7 @@ url = "https://github.com/kontur-1c/onec-grammarcheck"
 
 requires = ["pyaspeller", "Click", "junit-xml", "wasabi", "tqdm"]
 requires_test = ["pytest", "pytest-cov", "pytest-runner", "pytest-click"]
-requires_dev = requires_test + ["black", "flake8"]
+requires_dev = requires_test + ["black", "flake8", "semver"]
 
 setup(
     name=title,
